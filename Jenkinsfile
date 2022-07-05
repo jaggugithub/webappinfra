504c759181b4b42c3a9605ec0c5a1c3d1f66def2
jaggugithub <jaggu199@gmail.com>
2022-07-05 11:16:27 +0100

    Added Files

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage('Initialize Infrastructure') {
+            steps{
+                sh "terraform init"
+            }
+        }
+    }    
+}
--- ec2callingmodule.tf
+++ ec2callingmodule.tf
@@ -1,6 +1,7 @@
 module "ec2_calling_module" {
 
-  source = "git@github.com:jaggugithub/webappinfra.git//modules/ec2?ref=main"
+  source = "git::https://github.com/jaggugithub/webappinfra.git//modules/ec2?ref=main"
+  
 
   instance_count = var.instancecount
   ami_id         = data.aws_ami.ubuntu.id
