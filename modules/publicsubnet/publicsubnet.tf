9dd83998d840bd3546edfef9b3cabfea471671aa
jaggugithub <jaggu199@gmail.com>
2022-07-21 10:22:11 +0100

    Modified

--- modules/publicsubnet/publicsubnet.tf
+++ modules/publicsubnet/publicsubnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "webapp_publicsubnet" {
   vpc_id     = var.pubvpcid
   cidr_block = var.publicsubnet_cidr
-  map_public_ip_on_launch = true
+  #map_public_ip_on_launch = true
 
   tags = {
     Name = var.publicsubnet_name
