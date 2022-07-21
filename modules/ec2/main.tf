a43a0f2eba582d22da2130e88becfba1663daef7
jaggugithub <jaggu199@gmail.com>
2022-07-21 11:11:47 +0100

    Modified

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "webapp" {
 
-  count                  = var.instance_count
-  ami                    = var.ami_id
-  instance_type          = var.instancetype
-  key_name               = var.key
-  subnet_id              = var.publicsubnet_id
-  vpc_security_group_ids = var.sgid
+  count                       = var.instance_count
+  ami                         = var.ami_id
+  instance_type               = var.instancetype
+  key_name                    = var.key
+  subnet_id                   = var.publicsubnet_id
+  vpc_security_group_ids      = var.sgid
+  associate_public_ip_address = true
 
   tags = {
     Name        = var.servername[count.index]
