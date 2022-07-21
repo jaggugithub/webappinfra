d4aa5d8f3a584f079da4a29653f1324c50d42319
jaggugithub <jaggu199@gmail.com>
2022-07-21 10:31:25 +0100

    Modified

--- routetablecallingmodule.tf
+++ routetablecallingmodule.tf
@@ -17,10 +17,10 @@ module "rt_subnet_association" {
   rtsubassociate_id = module.routetable_module.routetable_id
 }
 
-module "rt_ig_association" {
+# module "rt_ig_association" {
 
-  source = "git::https://github.com/jaggugithub/webappinfra.git//modules/rtigasso?ref=main"
+#   source = "git::https://github.com/jaggugithub/webappinfra.git//modules/rtigasso?ref=main"
 
-  rtig_id          = module.webapp_ig.internet_gateway
-  rtigassociate_id = module.routetable_module.routetable_id
-}
+#   rtig_id          = module.webapp_ig.internet_gateway
+#   rtigassociate_id = module.routetable_module.routetable_id
+# }
