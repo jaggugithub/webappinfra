5ca98a012f8d388b166db8e6a9fd2f8840fdc713
jaggugithub <jaggu199@gmail.com>
2022-07-22 12:07:07 +0100

    Modified

--- routetablecallingmodule.tf
+++ routetablecallingmodule.tf
@@ -2,9 +2,9 @@ module "pub_routetable_module" {
 
   source = "git::https://github.com/jaggugithub/webappinfra.git//modules/routingtable?ref=main"
 
-  vpc_pubrt     = module.webapp_vpc.vpc_id
-  rt_pub_cidr    = var.publicsubnet_cidrblock
-  ig_pub_gateway = module.webapp_ig.internet_gateway
+  vpc_pubrt     = "${module.webapp_vpc.vpc_id}"
+  rt_pub_cidr    = "${var.publicsubnet_cidrblock}"
+  ig_pub_gateway = "${module.webapp_ig.internet_gateway}"
   rt_pub_name    = "${var.publicrtname}"
 
 }
@@ -13,18 +13,18 @@ module "public_rt_subnet_association" {
 
   source = "git::https://github.com/jaggugithub/webappinfra.git//modules/routingtable?ref=main"
 
-  pub_rtsub_id          = module.publicsubnet_webapp.publicsubnet_id
-  pub_rtsubassociate_id = module.pub_routetable_module.pub_routetable_id
+  pub_rtsub_id          = "${module.publicsubnet_webapp.publicsubnet_id}"
+  pub_rtsubassociate_id = "${module.pub_routetable_module.pub_routetable_id}"
 }
 
 module "private_routetable_module" {
 
   source = "git::https://github.com/jaggugithub/webappinfra.git//modules/routingtable?ref=main"
 
-  vpc_prirt     = module.webapp_vpc.vpc_id
-  rt_pri_cidr    = var.privatesubnet_cidrblock
-  nat_pri_gateway = module.webapp_nat.nat_gateway
-  rt_pri_name    = var.privatertname
+  vpc_prirt     = "${module.webapp_vpc.vpc_id}"
+  rt_pri_cidr    = "${var.privatesubnet_cidrblock}"
+  nat_pri_gateway = "${module.webapp_nat.nat_gateway}"
+  rt_pri_name    = "${var.privatertname}"
 
 }
 
@@ -32,6 +32,6 @@ module "private_rt_subnet_association" {
 
   source = "git::https://github.com/jaggugithub/webappinfra.git//modules/routingtable?ref=main"
 
-  pri_rtsub_id          = module.privatesubnet_webapp.privatesubnet_id
-  pri_rtsubassociate_id = module.private_routetable_module.pri_routetable_id
+  pri_rtsub_id          = "${module.privatesubnet_webapp.privatesubnet_id}"
+  pri_rtsubassociate_id = "${module.private_routetable_module.pri_routetable_id}"
 }
