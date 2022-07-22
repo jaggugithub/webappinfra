54f5784f85dab56eff2933c243e527a5688032c1
jaggugithub <jaggu199@gmail.com>
2022-07-22 10:59:13 +0100

    Modified

--- modules/routingtable/routingtable.tf
+++ modules/routingtable/routingtable.tf
@@ -1,34 +1,29 @@
 resource "aws_route_table" "webapp_pub_rt" {
   vpc_id = "${var.vpc_pubrt}"
-
   route {
     cidr_block = "${var.rt_pub_cidr}"
     gateway_id = "${var.ig_pub_gateway}"
   }
-
   tags = {
     Name = "${var.rt_pub_name}"
   }
 }
-
-resource "aws_route_table" "webapp_pri_rt" {
-  vpc_id = "${var.vpc_prirt}"
-
-  route {
-    cidr_block = "${var.rt_pri_cidr}"
-    gateway_id = "${var.nat_pri_gateway}"
-  }
-
-  tags = {
-    Name = "${var.rt_pri_name}"
-  }
-}
-
 resource "aws_route_table_association" "pub_webapp_rtassociate_subnet" {
   subnet_id      = "${var.pub_rtsub_id}"
   route_table_id = "${var.pub_rtsubassociate_id}"
 }
 
+
+resource "aws_route_table" "webapp_pri_rt" {
+  vpc_id = "${var.vpc_prirt}"
+  route {
+    cidr_block = "${var.rt_pri_cidr}"
+    gateway_id = "${var.nat_pri_gateway}"
+  }
+  tags = {
+    Name = "${var.rt_pri_name}"
+  }
+}
 resource "aws_route_table_association" "pri_webapp_rtassociate_subnet" {
   subnet_id      = "${var.pri_rtsub_id}"
   route_table_id = "${var.pri_rtsubassociate_id}"
--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -1,15 +1,15 @@
 output "publicsubnet_id" {
-    value = aws_subnet.webapp_publicsubnet.id
+    value = "${aws_subnet.webapp_publicsubnet.id}"
 }
 
 output "webapp_publicsubnet_cidr" {
-    value = aws_subnet.webapp_publicsubnet.cidr_block
+    value = "${aws_subnet.webapp_publicsubnet.cidr_block}"
 }
 
 output "privatesubnet_id" {
-    value = aws_subnet.webapp_privatesubnet.id
+    value = "${aws_subnet.webapp_privatesubnet.id}"
 }
 
 output "webapp_privatesubnet_cidr" {
-    value = aws_subnet.webapp_privatesubnet.cidr_block
+    value = "${aws_subnet.webapp_privatesubnet.cidr_block}"
 }
