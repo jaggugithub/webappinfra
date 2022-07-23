0a506c0e4180423ff873b8ec3a60fdd5c0ed4533
jaggugithub <jaggu199@gmail.com>
2022-07-23 22:24:23 +0100

    Modified

--- sgcallingmodule.tf
+++ sgcallingmodule.tf
@@ -6,6 +6,6 @@ module "SG_module" {
   name                 = var.SGName
   description          = var.SGdescription
   SG_Name              = var.sgname
-  sg_ingress_cidrblock = module.publicsubnet.webapp_publicsubnet_cidr
+  sg_ingress_cidrblock = "0.0.0.0/0" # module.publicsubnet.webapp_publicsubnet_cidr
 
 }
